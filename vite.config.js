81a82684f3b8e6081b5827b34cbde03734e082ba
Jay24op <jaymanmw2442@gmail.com>
2025-02-12 02:58:34 -0700

    vue to production 1.4

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://tsub.jaytech.pro/", //tsub.jaytech.pro Topsweets
+  base: "Topsweets/", //tsub.jaytech.pro Topsweets
   plugins: [vue()],
 });
