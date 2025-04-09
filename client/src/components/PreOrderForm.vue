7f8b4e49e461c86c900add17385d2198e51d7909
Jay24op <jaymanmw2442@gmail.com>
2025-04-09 18:45:14 -0500

    hero section crafted, pre-order-form component needs styling

---
+++ client/src/components/PreOrderForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="h-160 w-160 bg-white rounded-[3rem]">
+    <form class="pre-order-form">
+    <!-- <fieldset>'s' for line splitting and grouping -->
+      <h3 class="text-center mb-4">Pre-Order Form</h3>
+
+      <label for="product">Product:</label>
+      <select name="product_type" id="product_type">
+        <option value="">--select product--</option>
+        <option value="vanilla_cake">vanilla cake</option>
+        <option value="chocolate_cake">chocolate cake</option>
+        <option value="pecan_cookies">pecan cookies</option>
+      </select>
+
+      <label for="image">Photo or Sketch:</label>
+      <input type="file" name="image" id="image" />
+
+      <label for="name">Name:</label>
+      <input type="text" name="name" id="name" />
+
+      <label for="number">Contact Number:</label>
+      <input type="number" name="number" id="number" />
+
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" />
+
+      <label for="date">Event Date:</label>
+      <input type="date" name="date" id="date" />
+
+      <label for="notes">Additional Notes:</label>
+      <input type="text" name="notes" id="notes" />
+
+      <!-- add sumbit button -->
+    </form>
+  </section>
+</template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,10 +1,38 @@
 <template>
-  <div class="bg-blue h-50">hello world</div>
-  <h1>Routing in Progress!!!</h1>
-  <p>
-    Check out our
-    <router-link :to="{ name: 'Test' }">TEST ROUTE</router-link>!!!
-  </p>
+  <section class="hero-section">
+    <section class="hero-wrapper">
+      <section class="hero-content">
+        <div class="h-116 w-116">
+          <img
+            src="../assets/hero_logo.png"
+            alt="hero_logo"
+            class="size-full"
+          />
+        </div>
+        <div class="hero-message">
+          <h4>
+            Indulge in uniquely created treas for every occasion! From custom
+            cakes to delightful pastries, we bake with love and creativity just
+            for you!
+          </h4>
+        </div>
+      </section>
+
+      <PreOrderForm />
+    </section>
+  </section>
+  <h2 class="text-pink">
+    WELCOME ... THANK YOU FOR VISITING, WE ARE UNDER CONSTRUCTION!
+  </h2>
+  <h2 class="text-blue">I HOPE YOU ARE ENJOYING WHAT YOU SEE THUS FAR!</h2>
 </template>
 
+<script>
+import PreOrderForm from "../components/PreOrderForm.vue";
 
+export default {
+  components: {
+    PreOrderForm,
+  },
+};
+</script>
